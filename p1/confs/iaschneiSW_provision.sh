3e0b3f3102dca1aa4b933b99b377b83153368860
iaschnei <141677415+iaschnei@users.noreply.github.com>
2025-06-10 19:08:27 +0200

    Create iaschneiSW_provision.sh

---
+++ p1/confs/iaschneiSW_provision.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo apt-get update
+
+# Install kubectl through google-cli
+sudo apt-get install -y curl apt-transport-https ca-certificates gnupg ufw
+sudo ufw disable
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update
+sudo apt-get install -y google-cloud-cli
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+# Install k3s in agent mode
+curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN="12345" INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111 --flannel-iface=eth1" sh -s
+
+sleep 15
+
+#Setup config for kubectl
+sudo chmod 755 /etc/rancher/k3s/k3s.yaml
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
