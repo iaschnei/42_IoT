6611eb5ed73ff128d5fa38a9314cf2ef232a50a0
iaschnei <141677415+iaschnei@users.noreply.github.com>
2025-06-11 10:02:24 +0200

    Create iaschneiS_provision.sh

---
+++ p2/confs/iaschneiS_provision.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y curl apt-transport-https ca-certificates gnupg ufw
+sudo ufw disable
+
+#Install kubectl through google-cli
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update
+sudo apt-get install -y google-cloud-cli
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+#Install k3s in server mode
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --token 12345 --node-ip=192.168.56.110 --flannel-iface=eth1" sh -s
+
+sleep 15
+
+#Setup config for kubectl
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> /home/vagrant/.bashrc
+echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> /home/vagrant/.profile
