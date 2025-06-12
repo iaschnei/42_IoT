77f81730a58ed0ac009d83b978254a49cd144002
iaschnei <141677415+iaschnei@users.noreply.github.com>
2025-06-12 12:47:19 +0200

    Update iaschneiSW_provision.sh

--- p1/scripts/iaschneiSW_provision.sh
+++ p1/scripts/iaschneiSW_provision.sh
@@ -14,7 +14,17 @@ sudo apt-get install -y google-cloud-cli
 sudo apt-get update
 sudo apt-get install -y kubectl
 
-# Install k3s in agent mode
-curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN="12345" INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111 --flannel-iface=eth1" sh -s
+#Install k3s in agent mode (need multiple try, can randomly fail)
+for i in {1..3}; do
+  if curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN="12345" INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111 --flannel-iface=eth1" sh -s; then
+	break
+  else
+    if [ $i -eq 3 ]; then
+	echo "Failed to install k3s after 3 attempts"
+	exit 1
+    fi
+    sleep 10
+  fi
+done
 
 sleep 15
